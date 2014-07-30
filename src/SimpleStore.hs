1d182ba89c08145fec060375738b70901707f4a9
Kevin Cotrone <kevincotrone@gmail.com>
2014-07-30 11:56:01 -0500

    Initial ideas for modifiyng, getting, and putting a store

--- src/SimpleStore.hs
+++ src/SimpleStore.hs
@@ -9,11 +9,5 @@ module SimpleStore (
   , StoreError(..)
 ) where
 
-import           Control.Concurrent.STM.TMVar
-import           Control.Concurrent.STM.TVar
-import           Control.Monad.STM
-import           Data.Maybe                   (Maybe)
-import           SimpleStore.Internal
 import           SimpleStore.IO
 import           SimpleStore.Types
-import           System.IO                    (Handle)
--- src/SimpleStore/IO.hs
+++ src/SimpleStore/IO.hs
@@ -1,18 +1,15 @@
 module SimpleStore.IO where
 
-import           Control.Concurrent.STM.TMVar
 import           Control.Concurrent.STM.TVar
 import           Control.Monad.STM
-import           Data.Maybe                   (Maybe)
+import           SimpleStore.Internal
 import           SimpleStore.Types
-import           System.IO                    (Handle)
 
 getSimpleStore :: SimpleStore st -> IO st
 getSimpleStore store = atomically . readTVar . storeState $ store
 
 putSimpleStore :: SimpleStore st -> st -> IO ()
-putSimpleStore store state = atomically . (writeTVar tState) $ state
-  where tState = storeState store
+putSimpleStore store state = withLock store $ putWriteStore store state
 
 openSimpleStore :: FilePath -> IO (Either StoreError (SimpleStore st))
 openSimpleStore fp = undefined
@@ -24,4 +21,8 @@ closeSimpleStore :: SimpleStore st -> IO ()
 closeSimpleStore store = undefined
 
 modifySimpleStore :: SimpleStore st -> (st -> IO st) -> IO (Either StoreError ())
-modifySimpleStore store func = undefined
+modifySimpleStore store func = withLock store $ do
+  state <- atomically . readTVar . storeState $ store
+  state' <- func state
+  putSimpleStore store state'
+  return . Right $ ()
