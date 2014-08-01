49956dc8196ccebb2d18a9d26332e46039551adc
Kevin Cotrone <kevincotrone@gmail.com>
2014-08-01 11:04:23 -0500

    Adding locking to the making of a store

--- src/SimpleStore/IO.hs
+++ src/SimpleStore/IO.hs
@@ -60,6 +60,7 @@ openSimpleStore fp = do
 makeSimpleStore :: (S.Serialize st) => FilePath -> st -> IO (Either StoreError (SimpleStore st))
 makeSimpleStore dir state = do
   fp <- initializeDirectory dir
+  attemptTakeLock fp
   let encodedState = S.encode state
       checkpointPath = fp </> (fromText . pack $ (show initialVersion) ++ "checkpoint.st")
       initialVersion = 0
